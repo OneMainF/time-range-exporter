c3a07c3f4edd4167a10f6f2e7d09701bd414c524
Cody P Moore <cody.moore@omf.com>
2018-08-03 19:41:16 -0500

    Added comments

--- src/com/onemainfinancial/powersystems/prometheus/exporters/time/Definition.java
+++ src/com/onemainfinancial/powersystems/prometheus/exporters/time/Definition.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-import com.ibm.xylem.Logger;
-
 import io.prometheus.client.Collector.MetricFamilySamples;
 import io.prometheus.client.Collector.Type;
 
@@ -14,8 +12,10 @@ import io.prometheus.client.Collector.Type;
 public class Definition{
 	public Definition(HashMap<String,Object> props) {
 		name=Main.prefix+props.get("name").toString();
+		
+		//if not added yet, create MetricFamilySamples
 		if(!samples.containsKey(name)){
-			MetricFamilySamples sample=new MetricFamilySamples(name, Type.GAUGE, props.getOrDefault("help","").toString(), new  ArrayList<MetricFamilySamples.Sample>());
+			MetricFamilySamples sample=new MetricFamilySamples(name, Type.GAUGE, props.getOrDefault("help","").toString(), new ArrayList<MetricFamilySamples.Sample>());
 			samples.put(name,sample);	
 			metric_labels.put(name, new ArrayList<String>());
 		}
@@ -32,6 +32,7 @@ public class Definition{
 		
 		match=props.getOrDefault("match","all").toString().toLowerCase();
 		
+		//if labels are set, add to metric labels
 		if(props.containsKey("labels")) {
 			labels=(HashMap<String, Object>) props.get("labels");
 			for(String s:labels.keySet()) {
@@ -46,10 +47,11 @@ public class Definition{
 	String match;
 	String name;
 	HashMap<String,Object> labels=new HashMap<String,Object>();
-	
+	//use LinkedHashMap to retain order
 	static LinkedHashMap<String,MetricFamilySamples> samples=new LinkedHashMap<String,MetricFamilySamples>();
 	static HashMap<String,ArrayList<String>> metric_labels=new HashMap<String,ArrayList<String>>();
 	
+	//return a list of all MetricFamilySamlples
 	public static List<MetricFamilySamples> getList() {
 		List<MetricFamilySamples> list =new ArrayList<MetricFamilySamples>();
 		for(String s:samples.keySet()) {
@@ -57,22 +59,31 @@ public class Definition{
 		}
 		return list;
 	}
+	
+	//clear all samples and metrics
 	public static void reset() {
-		clear();
+		clearSamples();
 		samples.clear();
 		metric_labels.clear();
 	}
-	public static void clear() {
+	
+	//clear previous samples
+	public static void clearSamples() {
 		for(MetricFamilySamples sample:samples.values()) {
 			sample.samples.clear();
 		}
 	}
+	
+	//evaluate conditions
 	public void evaluate(HashMap<String,Object> bindings) {
+		//get labels for this metric
 		ArrayList<String> labelNames=metric_labels.get(name);
 		ArrayList<String> labelValues=new ArrayList<String>();
+		//get label values 
 		for(String s:labelNames) {
 			labelValues.add(labels.getOrDefault(s, "").toString());
 		}
+		
 		Integer value=0;
 		if(!match.equals("any")) {
 			value=1;
@@ -80,16 +91,19 @@ public class Definition{
 		
 		for(String condition:conditions) {
 			Boolean matches= Boolean.valueOf(Main.jinjava.render("{{"+condition+"}}",bindings));
+			//if 'any' and matches, set true
 			if(match.equals("any")) {
 				if(matches) {
 					value=1;
 					break;
 				}
+			//if 'none' and matches, set false
 			}else if(match.equals("none")) {
 				if(matches) {
 					value=0;
 					break;
 				}
+		    //if 'all' and not matches, set false
 			}else {
 				if(!matches) {
 					value=0;
@@ -97,6 +111,8 @@ public class Definition{
 				}
 			}	
 		}
+		
+		//add sample
 		samples.get(name).samples.add(new MetricFamilySamples.Sample(name,labelNames,labelValues, value));
 		
 		
--- src/com/onemainfinancial/powersystems/prometheus/exporters/time/Main.java
+++ src/com/onemainfinancial/powersystems/prometheus/exporters/time/Main.java
@@ -4,14 +4,10 @@ package com.onemainfinancial.powersystems.prometheus.exporters.time;
 
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.Instant;
-
-
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -30,37 +26,18 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.joda.time.DateTime;
-import org.joda.time.Weeks;
 import org.yaml.snakeyaml.Yaml;
 
 import com.hubspot.jinjava.Jinjava;
 
-
 import io.prometheus.client.Collector;
-import io.prometheus.client.Collector.MetricFamilySamples;
-import io.prometheus.client.Collector.Type;
 import io.prometheus.client.exporter.MetricsServlet;
 
 
 
 
 
-public class Main extends Collector {
-	public Main(HashMap<String,Object> config){
-		update(config);
-	}
-	public static void update(HashMap<String,Object> config) {
-		Definition.clear();
-		prefix=config.getOrDefault("prefix","time_range_").toString();
-		@SuppressWarnings({ "unchecked", "rawtypes" })
-		ArrayList<HashMap<String,Object>> metrics=(ArrayList) config.getOrDefault("metrics",new ArrayList<Object>());
-		for(HashMap<String,Object> metric:metrics) {
-		    definitions.add(new Definition(metric));	
-		}
-		
-		
-	}
-	static Server server=null;
+public class Main extends Collector {	
 	static Integer port=8080;
 	static Yaml yaml=new Yaml();
 	static Logger logger=Logger.getLogger(Main.class);
@@ -74,6 +51,7 @@ public class Main extends Collector {
 		bindings.put("environment", System.getenv());
 	}
 	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static HashMap<String,Object> readConfig(String filename){
 		Path path=Paths.get(filename);
 		HashMap<String,Object> returnData=new HashMap<String,Object>();
@@ -81,8 +59,11 @@ public class Main extends Collector {
 		try {
 			byte[] encoded = Files.readAllBytes(path);
 			String data=new String(encoded, Charset.defaultCharset());
-			//data=jinjava.render(data, bindings);
+			//evaluate any templating
+			data=jinjava.render(data, bindings);
+			//load to map
         	Object map=yaml.load(data);
+    		//if an array list, convert to map
         	if(map instanceof ArrayList) {
         		returnData.put("metrics", map);
         	}else {
@@ -91,6 +72,15 @@ public class Main extends Collector {
     	}catch(Exception e) {
     		logger.error("Could not load config",e);
     	}
+		
+		//Reset definitions in case of update
+		Definition.reset();
+		prefix=returnData.getOrDefault("prefix","time_range_").toString();
+		ArrayList<HashMap<String,Object>> metrics=(ArrayList) returnData.getOrDefault("metrics",new ArrayList<Object>());
+		//create definitions
+		for(HashMap<String,Object> metric:metrics) {
+		    definitions.add(new Definition(metric));	
+		}
 		return returnData;
 	}
 	
@@ -132,7 +122,8 @@ public class Main extends Collector {
 	        }
 
 	        
-	        
+	        // set console logger
+	        // may add option for log file later
 	        Properties log_props=new Properties();
 			log_props.put("log4j.rootLogger", "INFO, stdout");
 			log_props.put("log4j.appender.stdout","org.apache.log4j.ConsoleAppender");
@@ -141,7 +132,7 @@ public class Main extends Collector {
 			log_props.put("log4j.appender.stdout.layout.ConversionPattern","[%d{yyyy-MM-dd HH:mm:ss}] [%-5p] [%-20c{1}] %m%n");
 			PropertyConfigurator.configure(log_props);
 			
-	        
+	        //find config file
 	    	String file;
 	        if(System.getProperty("time.prometheus.exporter.config") !=null) {
 	        	file=System.getProperty("time.prometheus.exporter.config");	
@@ -152,7 +143,45 @@ public class Main extends Collector {
 	        }
 	        
 	        
-	        HashMap<String,Object> config=new HashMap<String,Object>();
+	        
+	   
+	        final File configFile=new File(file);
+	  
+	        if(!configFile.isFile()) {
+	        	logger.error("Could file does not exist or is not accessible");
+	        	System.exit(1);
+	        }
+	        
+	        HashMap<String,Object> config = readConfig(configFile.getAbsolutePath());
+	        
+	        
+	        //start file monitor thread
+	        new Thread(){
+		    	long lastMod=0;
+		    	public void run(){
+		    		lastMod=configFile.lastModified();
+		    	    while(true){
+		    	    	//check for updates to file
+		    	       try {
+		    			  Thread.sleep(5000);
+		    		   } catch (InterruptedException e) {
+		    			  break;
+		    		   }
+		    	       if(configFile.lastModified()!=lastMod){
+		    	    	   lastMod=configFile.lastModified();
+		    	    	   try {
+		    	    		 readConfig(configFile.getAbsolutePath());
+		    			   } catch (Exception e) {
+		    				 e.printStackTrace();
+		    		   }
+		    	     }
+		    	   }	    		
+		    	}
+		    }.start();
+		        
+	        
+	        
+	        //read port argument
 	        if(line.hasOption("port")) {
 		        try{
 		            port=Integer.parseInt(line.getOptionValue("port"));
@@ -160,95 +189,58 @@ public class Main extends Collector {
 		        	throw new Exception(line.getOptionValue("port")+" is not a valid port");
 		        }
 		        
-	        }
-	        final File configFile=new File(file);
-	        if(configFile.isFile()) {
-	        	config=readConfig(configFile.getAbsolutePath());
-	        	new Thread(){
-		    	    	long lastMod=0;
-		    	    	public void run(){
-		    	    		lastMod=configFile.lastModified();
-		    	    		while(true){
-		    	    		   try {
-		    					  Thread.sleep(5000);
-		    				   } catch (InterruptedException e) {
-		    					  break;
-		    				   }
-		    	    		   if(configFile.lastModified()!=lastMod){
-		    	    			   lastMod=configFile.lastModified();
-		    	    			   try {
-		    	    				 Main.update(readConfig(configFile.getAbsolutePath()));
-		    					   } catch (Exception e) {
-		    						 e.printStackTrace();
-		    					  }
-		    	    		   }
-		    	    		}	    		
-		    	    	}
-		    	    }.start();
-		        
-	        }else {
-	        	logger.error("Could file does not exist or is not accessible");
-	        	System.exit(1);
-	        }
-	        
-	        if(config.containsKey("port")) {
+	        }else if(config.containsKey("port")) {
+	        	//read config file port
 	        	port=Integer.valueOf(config.get("port").toString());
 	        }
-	       	        
-	        new Main(config).register();
-	        startServer();
+	       	//register collector and start   
+	        new Main().register();
+ 		    logger.info("Starting Jetty on port "+port);
+			Server server = new Server(port);
+		    ServletContextHandler context = new ServletContextHandler();
+		    context.setContextPath("/");
+		    server.setHandler(context);
+		    context.addServlet(new ServletHolder(new MetricsServlet()), "/metrics");
+		    server.start();
+		    server.join();
+		     
         }catch(Exception e){
         	System.out.println("Error: "+e.getMessage());
         	System.out.println("");
         }
         
 	}
-	
-	public static void startServer() throws Exception {
-		 logger.info("Starting Jetty on port "+port);
-		 server = new Server(port);
-	     ServletContextHandler context = new ServletContextHandler();
-	     context.setContextPath("/");
-	     server.setHandler(context);
-	     context.addServlet(new ServletHolder(new MetricsServlet()), "/metrics");
-	     server.start();
-	     server.join();
-	}
-
-
 
 
 	@Override
 	public List<MetricFamilySamples> collect() {
-		Definition.clear();
+		//clear last samples
+		Definition.clearSamples();
 
 		DateTime dt=DateTime.now();
-
+		
+		//week of month is not available in JodaTime
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(dt.toDate());
 		int weekOfMonth = calendar.get(Calendar.WEEK_OF_MONTH);
 		    
+		bindings.put("now", dt);
+		bindings.put("hour", dt.getHourOfDay());
+		bindings.put("minute", dt.getMinuteOfDay());
+		bindings.put("week", dt.getWeekOfWeekyear());
+		bindings.put("weekOfMonth",weekOfMonth);
+		bindings.put("dayOfMonth", dt.getDayOfMonth());
+		bindings.put("dayOfWeek", dt.getDayOfWeek());
+		bindings.put("dayOfYear", dt.getDayOfYear());
+		bindings.put("year", dt.getYear());
+		bindings.put("month", dt.getMonthOfYear());
 		
-		HashMap<String,Object> dateBindings=new HashMap<String,Object>();
-		dateBindings.put("now", dt);
-		dateBindings.put("hour", dt.getHourOfDay());
-		dateBindings.put("minute", dt.getMinuteOfDay());
-		dateBindings.put("week", dt.getWeekOfWeekyear());
-		dateBindings.put("weekOfMonth",weekOfMonth);
-		dateBindings.put("dayOfMonth", dt.getDayOfMonth());
-		dateBindings.put("dayOfWeek", dt.getDayOfWeek());
-		dateBindings.put("dayOfYear", dt.getDayOfYear());
-		dateBindings.put("year", dt.getYear());
-		dateBindings.put("month", dt.getMonthOfYear());
-		logger.info("Evaluating time "+dateBindings);
-		bindings.putAll(dateBindings);
-		
-		
+		//evaluate metrics
 		for(Definition definition:definitions) {
 		   definition.evaluate(bindings);   
 		}
 		
-		 
+		//return metric list
 		return Definition.getList();
 	}
 	
